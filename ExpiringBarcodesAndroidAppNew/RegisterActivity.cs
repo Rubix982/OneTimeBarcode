d6db48b33f2abc9dcdc536be9b5ea77d3b77a3d9
Saif Ul Islam <saifulislam84210@gmail.com>
2021-12-22 00:00:37 -0800

    Altered android app
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

---
+++ ExpiringBarcodesAndroidAppNew/RegisterActivity.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using ExpiringBarcodeDemo.Handler;
+using ExpiringBarcodesAndroidAppNew;
+
+namespace ExpiringBarcodeDemo
+{
+    [Activity(Label = "Register")]
+    public class RegisterActivity : Activity
+    {
+        private Api api;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            this.api = new Api(new User());
+            // Create your application here
+            SetContentView(Resource.Layout.Register);
+            
+            EditText username = FindViewById<EditText>(Resource.Id.username);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            EditText confirmpassword = FindViewById<EditText>(Resource.Id.confirmpassword);
+            Button btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+
+            btnRegister.Click += async (object sender, EventArgs e) =>
+            {
+                var res = await api.Register(username.Text, password.Text, confirmpassword.Text);
+                if (res.Error && res.ModelState.Any())
+                {
+                    Toast.MakeText(this, res.ModelState.First().Value.First(), ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Registration Complete", ToastLength.Short).Show();
+                    this.Finish();
+                }
+            };
+        }
+    }
+}
